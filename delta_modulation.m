bf3c8878aef0d6755e1c0c7391bc68a781d61e9e
nayem9b <razwanniam1@gmail.com>
2024-02-19 23:50:58 +0600

    delta modulation

--- delta_modulation.m
+++ delta_modulation.m
@@ -1,42 +1,47 @@
-clear all;
+clc;
 close all;
-
-A=5;
-f=1;
-theta=0;
-minTime=0;
-maxTime=1;
-samplePerTime=100;
-t=minTime : 1/samplePerTime;
-analogSignal= A*sin(2*pi*f*t +theta);
-figure;
-plot(t,analogSignal);
-disp(analogSignal);
-
-samplingRate=30*f;
-sampleTime=minTime :1/samplingRate: maxTime;
-sampledSignal=A*sin(2*pi*f*sampleTime +theta);
-
-digitalData=[];
-stairCaseSignal=0;
-delta=0.3;
-
-for i=1:1:(samplingRate*maxTime)
-    if sampledSignal(i)>stairCaseSignal(i)
-        digitalData(i+1)=1;
-        stairCaseSignal(i+1)=stairCaseSignal(i)+delta;
-    else
-        digitalData(i+1)=0;
-        stairCaseSignal(i+1)=stairCaseSignal(i)-delta;
-    end
-end
-
-disp(digitalData);
-disp(stairCaseSignal);
-subplot(4,1,2);
-stem(sampleTime, sampledSignal);
-xlabel("Time(sec)");
-ylabel("Sampled Signal");
-grid on;
+clear all;
+A = 8;
+f = 2;
+phi = 0;
+tmin = 0;
+tmax = 1;
+samplepertime = 100;
+t = tmin: 1/samplepertime: tmax;
+analogsignal = A.*sin(2*pi*f*t + phi);
+subplot(2,1,1);
 hold on;
-disp(sampledSignal);
+plot(t,analogsignal);
+axis([tmin-0.05 tmax+0.05 -A-1 +A+1]);
+title('Analog signal');
+samplingrate = 80*f;
+sampletime = tmin: 1/samplingrate: tmax;
+sampledsignal = A*sin(2*pi*f*sampletime);
+delta = 1.25;
+digitaldata = [];
+staircasesignal = 0;
+for i=1:1:(samplingrate*tmax)
+if sampledsignal(i)>staircasesignal(i)
+digitaldata(i+1) = 1;
+staircasesignal(i+1)=staircasesignal(i)+delta;
+else digitaldata(i+1) = 0;
+staircasesignal(i+1)=staircasesignal(i)-delta;
+end
+end
+stairs(sampletime,staircasesignal);
+hold on;
+ax = gca;
+ax.XTick = [0:1/samplingrate:tmax];
+ax.YTick = [(-A+2.5): delta: (A+2.5)];
+xlabel('Time(sec)');
+ylabel('Amplitude (volts)');
+legend('Analog input signal', 'staircase signal');
+title('Delta modulation');
+digitaldatalength = length(digitaldata);
+bittime = 0: tmax/(digitaldatalength-1): tmax;
+subplot(2,1,2);
+stairs(bittime,digitaldata);
+axis([0 tmax -0.5 1.5]);
+xlabel('Time(sec)');
+ylabel('Amplitude (volts)');
+title('Digital Signal');
