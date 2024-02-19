1547b41103ec0716c000a49794ea1335b04e7200
nayem9b <razwanniam1@gmail.com>
2024-02-19 22:33:41 +0600

    pulse amplitude modulation

---
+++ binary-amp-shift-keying.m
@@ -0,0 +1,21 @@
+for i = 1:1:n
+    if bitstream(i)==1
+        digitalsignalvotage(i) =highlogiclevelvoltage
+    end
+    else
+     digitalsignalvotage(i) =highlogiclevelvoltage
+end
+end
+
+totalsample=1:1:(n+1)*sampleperbit
+
+for i = 1:1:n
+    for j = i:1/sampleperbit:i+1
+        digitalsignal(totalsample())
+        
+
+    end
+end
+gigitalsignal(i)=highlogiclevelvoltage
+totalsample=1:1:(n+1)*sampleperbit
+amplitudecomponent=1;
