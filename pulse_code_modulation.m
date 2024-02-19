e47b6e70608aae06c518e57e7b8402ee882cf217
nayem9b <razwanniam1@gmail.com>
2024-02-19 23:56:01 +0600

    pulse code modulation

--- pulse_code_modulation.m
+++ pulse_code_modulation.m
@@ -1,63 +1,59 @@
 clc;
 clear all;
 close all;
-
-A = 5;
-f = 1;
-minTime = 0;
-maxTime = 1;
-theta = 0;
-samplePerTime = 100;
-t = minTime : 1/samplePerTime : maxTime;
-analogSignal = A*sin(2*pi*f*t + theta);
-subplot(4,1,1);
-plot(t , analogSignal);
-axis([minTime maxTime -A-1 A+1]);
-xlabel('Time (sec)');
-ylabel('Amplitude (volts)');
+%plotting the analog signal
+v = 8;
+f =2;
+phi = 0;
+maxtime = 1;
+samplerate = 1000;
+t = 0:1/(samplerate-1) : maxtime;
+analogsignal = v*sin(2*pi*f*t+phi);
+subplot(2,2,1);
+plot(t, analogsignal);
 title('Analog Signal');
-grid on;
-
-samplingRate = 30*f;
-totalSample = minTime : 1/samplingRate : maxTime;
-sampledSignal = A*sin(2*pi*f*totalSample + theta);
-
-subplot(4,1,2);
-stem(totalSample ,sampledSignal );
-xlabel('Time (sec)');
-ylabel('Amplitude (volts)');
+axis([0, maxtime, -v-1, +v+1]);
+xlabel('time(sec)');
+ylabel('Amplitude');
+%sampling process and plotting the sampled signal
+samplingrate = 10*f;
+dtime = 0: 1/samplingrate: maxtime;
+sampledsignal = v*sin(2*pi*f*dtime+phi);
+subplot(2,2,2);
+stem(dtime, sampledsignal);
 title('Sampled Signal');
-grid on;
-%disp(sampledSignal);
-
+axis([0, maxtime, -v-1, +v+1]);
+xlabel('time(sec)');
+ylabel('Amplitude');
+%quantization of the sampled signal and plotting it
 n = 8;
-L = 2^n;
-minV = -A;
-maxV = +A;
-delta = (maxV - minV)/L;
-partition = minV : delta : maxV;
-%disp(partition);
-codebook = minV - (delta/2) : delta : maxV + (delta/2);
-%disp(codebook);
-[index,quants] = quantiz(sampledSignal,partition,codebook);
-%disp(index);
-%disp(quants);
-subplot(4,1,3);
-stem(totalSample ,sampledSignal );
-xlabel('Time (sec)');
-ylabel('Amplitude (volts)');
+L = 2.*n;
+vmin = -v;vmax = +v;
+stepsize = (2*v)/L;
+partition = vmin: stepsize: vmax;
+codebook = vmin-(stepsize/2): stepsize: vmax+(stepsize/2);
+[index,damplitude] = quantiz(sampledsignal,partition,codebook);
+subplot(2,2,3);
+stem(dtime, damplitude);
+axis([0, maxtime, -v-1, +v+1]);
+xlabel('time(sec)');
+ylabel('Amplitude');
 title('Quantized Signal');
-grid on;
-
+%encoding process and the output is a matrix of 0's and 1's
 codematrix = de2bi(index,'left-msb');
-k = 1;
-for i=1:1:length(index);
-    for j=1:1:n
-        codevector(k)=codematrix(i,j);
-        k=k+1;
-    end
+%making a vector form of the encoded matrix bit stream
+[row,coloumn] = size(codematrix);
+k=1;
+for i=1:1:row
+for j=1:1:coloumn
+codevector(k) = codematrix(i,j);
+k = k+1;
 end
-disp(codevector);
-subplot(4,1,4);
-stairs(codevector);
-
+end
+%plotting the digital signal
+codevectorlength = length(codevector);
+T = 0: maxtime/(codevectorlength-1): maxtime;
+subplot(2,2,4);
+stairs(T,codevector);
+title('Digital Signal');
+axis([0 maxtime -0.5 1.5]); xlabel('Time(sec)');
