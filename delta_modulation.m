48505bbbcdb51ab5f9fac28a1e6d981eae0a2339
nayem9b <razwanniam1@gmail.com>
2024-02-21 15:08:52 +0600

    delta modulation fixes

--- delta_modulation.m
+++ delta_modulation.m
@@ -1,47 +1,64 @@
+% Clear command window, workspace, and close all figures
 clc;
-close all;
 clear all;
-A = 8;
-f = 2;
-phi = 0;
-tmin = 0;
-tmax = 1;
-samplepertime = 100;
-t = tmin: 1/samplepertime: tmax;
-analogsignal = A.*sin(2*pi*f*t + phi);
+close all;
+ 
+% Define parameters for analog signal
+A = 5;            
+f = 1;           
+theta = 0;      
+minTime = 0;    
+maxTime = 2;   
+samplePerTime = 100;
+t = minTime : 1/samplePerTime : maxTime; % Time vector for analog signal
+ 
+% Generate analog signal (sine wave)
+analogSignal = A * sin(2*pi*f*t + theta);
+figure;
 subplot(2,1,1);
-hold on;
-plot(t,analogsignal);
-axis([tmin-0.05 tmax+0.05 -A-1 +A+1]);
-title('Analog signal');
-samplingrate = 80*f;
-sampletime = tmin: 1/samplingrate: tmax;
-sampledsignal = A*sin(2*pi*f*sampletime);
-delta = 1.25;
-digitaldata = [];
-staircasesignal = 0;
-for i=1:1:(samplingrate*tmax)
-if sampledsignal(i)>staircasesignal(i)
-digitaldata(i+1) = 1;
-staircasesignal(i+1)=staircasesignal(i)+delta;
-else digitaldata(i+1) = 0;
-staircasesignal(i+1)=staircasesignal(i)-delta;
+plot(t , analogSignal);
+axis([minTime maxTime -A-1 A+1]); 
+hold on; 
+ 
+% Calculate the sampling rate and create time vector for discrete sampling
+samplingRate = 30*f;
+sampleTime = minTime : 1/samplingRate : maxTime;
+ 
+% Sample the analog signal
+sampledSignal = A * sin(2*pi*f*sampleTime + theta);
+ 
+% Initialize variables for digital signal conversion
+digitalData = [];
+staircaseSignal = 0;
+delta = 1;
+ 
+% Convert the sampled signal to digital data using staircase approximation
+for i = 1:1:(samplingRate*maxTime)
+    if sampledSignal(i) > staircaseSignal(i)
+        digitalData(i+1) = 1;
+        staircaseSignal(i+1) =  staircaseSignal(i) + delta;
+    else
+        digitalData(i+1) = 0;
+        staircaseSignal(i+1) = staircaseSignal(i) - delta;
+    end
 end
-end
-stairs(sampletime,staircasesignal);
-hold on;
+ 
+% Plot the staircase approximation of the sampled signal
+stairs(sampleTime, staircaseSignal);
 ax = gca;
-ax.XTick = [0:1/samplingrate:tmax];
-ax.YTick = [(-A+2.5): delta: (A+2.5)];
-xlabel('Time(sec)');
+ax.XTick = [0:1/samplingRate:maxTime]; 
+ax.YTick = [-(A+0.1):delta:(A+0.1)];  
+xlabel('Time (sec)');  
 ylabel('Amplitude (volts)');
-legend('Analog input signal', 'staircase signal');
-title('Delta modulation');
-digitaldatalength = length(digitaldata);
-bittime = 0: tmax/(digitaldatalength-1): tmax;
+title('Digital Signal'); 
+grid on;
+ 
+% Calculate time vector for digital data
+digitalDataLength = length(digitalData);
+t1 = 0 : maxTime / (digitalDataLength-1) : maxTime;
+ 
+% Plot the digital output signal
 subplot(2,1,2);
-stairs(bittime,digitaldata);
-axis([0 tmax -0.5 1.5]);
-xlabel('Time(sec)');
-ylabel('Amplitude (volts)');
-title('Digital Signal');
+stairs(t1, digitalData);
+axis([minTime-0.05 maxTime+0.05 -0.1 +1.1]); 
+title('Digital Output Signal');
